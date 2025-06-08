fbe1533bc793240288df1bc632cc7af8688d4e71
Marco Geremia <github@geremia.dev>
2025-06-08 16:53:48 +0200

    first commit

---
+++ src/main.c
@@ -0,0 +1,127 @@
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+void frameBufferSizeCallback(GLFWwindow *window, int width, int height) { glViewport(0, 0, width, height); }
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+
+  static bool wKeyPressed = false;
+  if ((glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) && !wKeyPressed) {
+    static GLenum mode = GL_FILL;
+    mode = mode == GL_LINE ? GL_FILL : GL_LINE;
+    glPolygonMode(GL_FRONT_AND_BACK, mode);
+  }
+
+  wKeyPressed = (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) ? true : false;
+}
+
+int main() {
+  glfwInit();
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    printf("Failed to create GLFW window\n");
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, frameBufferSizeCallback);
+
+  GLfloat rectangle_vp[] = {0.5f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f, -0.5f, 0.5f, 0.0f};
+
+  GLuint indices[] = {0, 1, 3, 1, 2, 3};
+
+  const char *vertexShaderSource = "#version 330 core\n"
+                                   "layout (location = 0) in vec3 aPos;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   gl_Position = vec4(aPos, 1.0);\n"
+                                   "}\0";
+
+  const char *fragmentShaderSource = "#version 330 core\n"
+                                     "out vec4 FragColor;\n"
+                                     "void main()\n"
+                                     "{\n "
+                                     "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                     "}\0";
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+
+  glCompileShader(vertexShader);
+  int success;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    printf("Vertex compilation failed\n");
+    return -1;
+  }
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+
+  glCompileShader(fragmentShader);
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    printf("Fragment compilation failed\n");
+    return -1;
+  }
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    printf("Shader program linkage failed\n");
+    return -1;
+  }
+
+  glUseProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  unsigned int EBO;
+  unsigned int VAO;
+  unsigned int VBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &EBO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle_vp), rectangle_vp, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    processInput(window);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  return 0;
+}
